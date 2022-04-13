863ea76e4947994b3637a5e8a5b001e5d0610d52
alvarosh15 <alvaro.s.h.15@gmail.com>
2022-04-13 16:56:45 +0200

    Added components folder and login component

---
+++ mynimalist/src/componentes/LogoWriting.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../hojas-de-estilo/LogoWriting.css";
+
+function LogoWriting() {
+  return (
+    <div class="wrapper">
+      <div class="typing-demo">{'<Mynimalist. />'}</div>
+    </div>
+  );
+}
+
+export default LogoWriting;
