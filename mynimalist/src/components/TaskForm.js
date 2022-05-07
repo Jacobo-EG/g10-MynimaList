532da1859dcc416e158f2ea75ed19a3d84b6562b
Jacobo-EG <0619975360@uma.es>
2022-05-07 17:33:07 +0200

    Avanzado en el desarrollo de TaskForm.js, quedo a la espera de que se avance en el desarrollo de otros ficheros relacionados con los tasks para poder comprobar el funcionamiento

--- mynimalist/src/components/TaskForm.js
+++ mynimalist/src/components/TaskForm.js
@@ -1,18 +1,57 @@
-import React from 'react'
+import React from "react";
+import "../styles/AddNewTask.css";
+import TaskForm from "./TaskForm";
+import { TaskContext } from "../context";
+import { Palette } from "react-bootstrap-icons";
 
-function TaskForm({handleSubmit, heading, confirmText}){
+function TaskForm({
+    handleSubmit,
+    heading = false,
+    text, setText,
+    lists,
+    showButtons = false,
+    setShowModal = false
+}) {
 
-    return (
-        <form className='TaskForm' onSubmit={handleSubmit}>
-            <h3>{heading}</h3>
-            <input
-            type="text"
-            autoFocus/>
-            <button className='confirm'>
-                {confirmText}
-            </button>
-        </form>
-    )
+  function handleSubmit(e) {
+
+  }
+
+  useEffect( () => {
+    setTaskList(selectedList)
+  }, [selectedList])
+
+  return (
+    <form className='TaskForm' onSubmit={handleSubmit}>
+        <div className="text">
+                {
+                    heading &&
+                    <h3>{heading}</h3>
+                }
+                <input
+                type="text"
+                autoFocus/>
+                <button className='confirm'>
+                    {confirmText}
+                </button>
+        </div>
+        <div className="pick-project">
+            <div className="title">
+                <Palette />
+                <p>Elige una lista</p>
+            </div>
+            <div className="lists">
+                {
+                    lists.map( list =>
+                        <div className="list" key={list.id}>
+                            {list.name}
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    </form>
+  );
 }
 
-export default TaskForm
+export default TaskForm;
