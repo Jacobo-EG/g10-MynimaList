66816a5b4c860969a5d9e1ace1f2b118b060a6dc
Jesús Escudero Moreno <77730984+xexu65@users.noreply.github.com>
2022-05-13 11:35:29 +0200

    Update MynimalistdemoApplicationTests.java

--- MynimalistAPI/src/test/java/com/example/mynimalist/MynimalistdemoApplicationTests.java
+++ MynimalistAPI/src/test/java/com/example/mynimalist/MynimalistdemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.mynimalist;
 
+import java.beans.Transient;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +11,85 @@ class MynimalistdemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test 
+	void crearUsuario(){
+
+	}
+
+	@Test 
+	void numListas0(){
+
+
+	}
+
+
+
+	@Test 
+	void numTareas0(){
+
+	}
+
+
+
+	@Test 
+	void inicioSesion(){
+
+	}
+
+
+
+	@Test 
+	void crearLista(){
+
+	}
+
+
+
+	@Test 
+	void editarLista(){
+
+	}
+
+
+
+	@Test 
+	void eliminarLista(){
+
+	}
+
+
+
+
+	@Test 
+	void crearTarea(){
+
+	}
+
+
+
+
+	@Test 
+	void editarTarea(){
+
+	}
+
+
+
+
+
+	@Test 
+	void eliminarTodasTareas(){
+
+	}
+
+
+
+	@Test 
+	void eliminarTarea(){
+
+	}
+
+
 
 }
