babdbd5416ffa97cc6fe96ac4201c0498595b4a1
juanmagdev <me@juanmagd.dev>
2022-05-17 00:28:19 +0200

    AboutUs Image Solved

--- mynimalist/src/pages/AboutUs.js
+++ mynimalist/src/pages/AboutUs.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
 import '../styles/AboutUs.css';
-//import logo from './AboutImage.jpg'
-
+import aboutImage from './AboutImage.png'
 function AboutUs () {
     let navigate = useNavigate();
     const handleClickSignUp = () => {
@@ -11,13 +10,18 @@ function AboutUs () {
   
     return (
         <div className="about-container">
-            Sobre nosotros: 
-
-            Somos un grupo de estudiantes de la doble titulación de Matemáticas e Ingeniería Informática en la Universidad de Málaga.
-            BlaBlaBla 
-
-            Insertar imagen aqui            
-
+            <h1>{"Sobre nosotros"}</h1>
+            <br>{}</br>
+            <i>MynimaList</i> nace como una solución simple y efectiva para todos aquellos que quieren generar listas de tareas de forma minimalista y rápida.
+            <br>{}</br>
+            <br>{}</br>
+            <i>MynimaList</i> ha sido desarrollada por un grupo de alumnos del Doble Grado de Ingienería Informática + Matemáticas de la Universidad de Málaga, como 
+            proyecto final a la asignatura de <i>Introducción a la Ingienería del Software</i>, donde hemos aprendido tanto metodologías de modelado de software como 
+            Scrum, a documentar el proyecto de una forma mas completa y parecida a lo que se hace en la realidad y además, hemos tenido un primer contacto con tecnologías 
+            como HTML, CSS, React, Java orientado al back-end, Git...
+            <div className="col-2">
+                <img src={aboutImage} alt="Working People"></img>          
+            </div>
         </div>
     );
     }
