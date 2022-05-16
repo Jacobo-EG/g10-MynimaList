296ef8a4cb42a3507ce020db51e4dd4e8740e973
alvarosh15 <alvaro.s.h.15@gmail.com>
2022-05-16 12:06:19 +0200

    Added email regular expresion, min length for user and pass, and wrong user or pass error on login

--- mynimalist/src/pages/SignUp.js
+++ mynimalist/src/pages/SignUp.js
@@ -13,7 +13,7 @@ function SignUp() {
   const [usernameCorrecto, setUsernameCorrecto] = useState(true);
   const [passwordCorrecta, setPasswordCorrecta] = useState(true);
 
-  const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   let navigate = useNavigate();
   const handleClickSignIn = () => {
