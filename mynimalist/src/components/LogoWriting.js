595c8ac0f373bba73cb66de47eeeabcf2b68ab0b
alvarosh15 <alvaro.s.h.15@gmail.com>
2022-05-18 13:30:41 +0200

    tried to change logo on tab

--- mynimalist/src/components/LogoWriting.js
+++ mynimalist/src/components/LogoWriting.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "../styles/LogoWriting.css";
 
+
 function LogoWriting() {
+
   return (
     <div className="wrapper">
       <div className="typing-demo">{'MynimaList'}</div>
--- mynimalist/src/pages/AboutUs.js
+++ mynimalist/src/pages/AboutUs.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import '../styles/AboutUs.css';
 import aboutImage from './AboutImage.png'
+import { ArrowLeftSquareFill } from "react-bootstrap-icons";
+
 function AboutUs () {
+
+
+  const [checked, setChecked] = useState(localStorage.getItem("theme") === "dark" ? true : false);
+
     let navigate = useNavigate();
     const goBack = () => {
         navigate('/');
@@ -10,17 +16,22 @@ function AboutUs () {
   
     return (
         <div className="about-container">
-            <h1>Sobre nosotros</h1>
-            <br>{}</br>
-            <i>MynimaList</i> nace como una solución simple y efectiva para todos aquellos que quieren generar listas de tareas de forma minimalista y rápida.
-            <br>{}</br>
-            <br>{}</br>
-            <i>MynimaList</i> ha sido desarrollada por un grupo de alumnos del Doble Grado de Ingienería Informática + Matemáticas de la Universidad de Málaga, como 
-            proyecto final para la asignatura de <i>Introducción a la Ingienería del Software</i>, donde hemos aprendido tanto metodologías de modelado de software como 
-            Scrum, a documentar el proyecto de una forma más completa y parecida a lo que se hace en la realidad y además, hemos tenido un primer contacto con tecnologías 
-            como HTML, CSS, React, Java orientado al back-end, Git...
-            <div className="col-2">
-                <img src={aboutImage} alt="Working People"></img>          
+            <span className="back" onClick={() => goBack()}>
+                <ArrowLeftSquareFill color={checked ? "white" : "black"}  />
+            </span>
+            <div>
+                <h1>Sobre nosotros</h1>
+                <br>{}</br>
+                <i>MynimaList</i> nace como una solución simple y efectiva para todos aquellos que quieren generar listas de tareas de forma minimalista y rápida.
+                <br>{}</br>
+                <br>{}</br>
+                <i>MynimaList</i> ha sido desarrollada por un grupo de alumnos del Doble Grado de Ingienería Informática + Matemáticas de la Universidad de Málaga, como 
+                proyecto final para la asignatura de <i>Introducción a la Ingienería del Software</i>, donde hemos aprendido tanto metodologías de modelado de software como 
+                Scrum, a documentar el proyecto de una forma más completa y parecida a lo que se hace en la realidad y además, hemos tenido un primer contacto con tecnologías 
+                como HTML, CSS, React, Java orientado al back-end, Git...
+                <div className="col-2">
+                    <img src={aboutImage} alt="Working People"></img>          
+                </div>
             </div>
         </div>
     );
